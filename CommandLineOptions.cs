69944de2fdc59c44dd3bf4e7d35937afde4e10f4
Elias Puurunen <elias@northernhci.ca>
2023-11-13 16:24:00 -0500

    Add project files.

---
+++ CommandLineOptions.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+
+public class CommandLineOptions
+{
+    public CommandLineOptions(int width, int height, string name, int frameRate, bool whiteLine, string mode)
+    {
+        this.Width = width;
+        this.Height = height;
+        this.Name = name;
+        this.FrameRate = frameRate;
+        this.WhiteLine = whiteLine;
+        this.Mode = mode;
+    }
+
+    public CommandLineOptions()
+    {
+    }
+
+    [Option('w', "width", Required = true, HelpText = "Horizontal resolution in pixels")]
+    public int Width { get; set; }
+
+    [Option('h', "height", Required = true, HelpText = "Vertical resolution in pixels")]
+    public int Height { get; set; }
+
+    [Option('n', "name", Required = true, HelpText = "Name of this NDI sender instance")]
+    public string Name { get; set; }
+
+    [Option('f', "fps", Required = false, HelpText = "Frames per second.", Default = 30)]
+    public int FrameRate { get; set; }
+
+    [Option(longName: "whiteline", Required = false, HelpText = "Display the sweeping white line", Default = false)]
+    public bool WhiteLine { get; set; }
+
+    [Option(longName: "mode", Required = false, HelpText = "Background mode. Expecting 'blue', 'noise'", Default = "blue")]
+    public string Mode { get; set; }
+
+}
